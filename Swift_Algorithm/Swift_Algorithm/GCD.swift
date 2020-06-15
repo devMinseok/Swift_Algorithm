cfad748c1b0fa27704adefb4d5d4835fb3d7939e
KangMinseok <qbq5000@gmail.com>
2020-06-15 21:04:39 +0900

    Add GCD

---
+++ Swift_Algorithm/Swift_Algorithm/GCD.swift
@@ -0,0 +1,20 @@
+//
+//  GCD.swift
+//  Swift_Algorithm
+//
+//  Created by 강민석 on 2020/06/02.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import Foundation
+
+//var firstNum = Int(readLine()!)!
+//var secondNum = Int(readLine()!)!
+//
+//while secondNum != 0 {
+//    let num = firstNum % secondNum
+//    firstNum = secondNum
+//    secondNum = num
+//}
+//
+//print("최대공약수: \(firstNum)")
