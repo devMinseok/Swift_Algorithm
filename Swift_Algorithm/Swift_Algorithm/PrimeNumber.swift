7c99095be8c457482647d993e93bc8007bf33c53
KangMinseok <qbq5000@gmail.com>
2020-06-15 21:04:00 +0900

    Add PrimeNumber

---
+++ Swift_Algorithm/Swift_Algorithm/PrimeNumber.swift
@@ -0,0 +1,30 @@
+//
+//  PrimeNumber.swift
+//  Swift_Algorithm
+//
+//  Created by 강민석 on 2020/06/03.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import Foundation
+
+//var isPrime = true
+//var primeNumber = [Int]()
+//
+//for i in 2...100 {
+//    isPrime = true
+//    for j in 2..<i {
+//        if i % j == 0 {
+//            isPrime = false
+//            break
+//        }
+//        continue
+//    }
+//    if isPrime {
+//        primeNumber.append(i)
+//    } else if primeNumber.count == 10 {
+//        break
+//    }
+//}
+//
+//print(primeNumber)
