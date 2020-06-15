85f04601cda01e102455fbaeb504fe2f044a80b9
KangMinseok <qbq5000@gmail.com>
2020-06-15 21:03:37 +0900

    Add sort algorithm

---
+++ Swift_Algorithm/Swift_Algorithm/sort.swift
@@ -0,0 +1,23 @@
+//
+//  sort.swift
+//  Swift_Algorithm
+//
+//  Created by 강민석 on 2020/06/02.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import Foundation
+
+//var arr: [Int] = []
+//
+//var inputCount = Int(readLine()!)!
+//
+//for _ in 1...inputCount {
+//    arr.append(Int(readLine()!)!)
+//}
+//
+//arr.sort(by: <)
+//
+//for i in arr {
+//    print(i)
+//}
