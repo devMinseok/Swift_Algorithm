8f2a4ca487b088cdff7f884bc2943d4e38330027
KangMinseok <qbq5000@gmail.com>
2020-06-15 21:04:52 +0900

    Add Fibonacci

---
+++ Swift_Algorithm/Swift_Algorithm/Fibonacci.swift
@@ -0,0 +1,21 @@
+//
+//  Fibonacci.swift
+//  Swift_Algorithm
+//
+//  Created by 강민석 on 2020/06/03.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import Foundation
+
+//var count = Int(readLine()!)!
+//var temp1 = 1
+//var temp2 = 0
+//var result = 0
+//
+//for i in 1...count {
+//    print(temp1)
+//    result = temp1 + temp2
+//    temp2 = temp1
+//    temp1 = result
+//}
