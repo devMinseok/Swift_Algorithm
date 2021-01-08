35fa26cbd39684836afef1613a633a9ceaae526e
KangMinseok <qbq5000@gmail.com>
2021-01-08 22:32:12 +0900

    ✏️ Do it 알고리즘: 02 기본 자료구조 2일차

--- Swift_Algorithm/Swift_Algorithm/main.swift
+++ Swift_Algorithm/Swift_Algorithm/main.swift
@@ -33,7 +33,7 @@ func maxValue() {
 
 //maxValue()
 
-// MARK: - 배열의 요소를 역순으로
+// MARK: - 61p 연습문제 Q2 / 배열의 요소를 역순으로
 func reverse() {
     print("배열의 크기는?: ", terminator: "")
     let length = Int(readLine()!)!
@@ -61,3 +61,72 @@ func reverse() {
 }
 
 //reverse()
+
+// MARK: - 61p 연습문제 Q3 / 배열의 모든 요소의 합계 구하기
+func sumOf() {
+    
+    var arr = [Int]()
+    
+    print("10개의 요소를 입력해주세요.")
+    for _ in 1...10 {
+        arr.append(Int(readLine()!)!)
+    }
+    
+    var sum = 0
+    for i in arr {
+        sum += i
+    }
+    
+    print("입력하신 요소들의 합은 \(sum)입니다.")
+}
+
+//sumOf()
+
+// MARK: - 64p 연습문제 Q4 / 배열 a의 모든 요소를 배열 b에 복사
+func copy() {
+    var a = [Int]()
+    var b = [Int]()
+    
+    print("배열의 크기는?: ", terminator: "")
+    let length = Int(readLine()!)!
+    
+    for _ in 0..<length {
+        a.append(Int.random(in: 1...100))
+    }
+    
+    print("a 배열: \(a)")
+    
+    for i in a {
+        b.append(i)
+    }
+    
+    print("b 배열: \(b)")
+}
+
+//copy()
+
+// MARK: - 64p 연습문제 Q5 / 배열 a의 모든 요소를 배열 b에 역순으로 복사
+
+func rcopy() {
+    var a = [Int]()
+    var b = [Int]()
+    
+    print("배열의 크기는?: ", terminator: "")
+    let length = Int(readLine()!)!
+    
+    for _ in 0..<length {
+        a.append(Int.random(in: 1...100))
+    }
+    
+    print("a 배열: \(a)")
+    
+    for i in 0..<length {
+        b.append(a[length - i - 1])
+    }
+    
+    print("b 배열: \(b)")
+}
+
+//rcopy()
+
+// MARK: -
