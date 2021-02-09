38c7966169407ae7a681a6dd63ed1e4423aecec0
KangMinseok <qbq5000@gmail.com>
2021-02-09 23:55:06 +0900

    ✏️ Do it 알고리즘: 09 리스트 1일차

--- Swift_Algorithm/Swift_Algorithm/main.swift
+++ Swift_Algorithm/Swift_Algorithm/main.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+class LinkedList<T> {
+    class Node<T> {
+        private var data: T // 데이터
+        private var next: Node<T> // 뒤쪽 포인터(다음 노드 참조)
+        
+        init(data: T, next: Node<T>) {
+            self.data = data
+            self.next = next
+        }
+    }
+    
+    private var head: Node<T> // 머리 노드
+    private var crnt: Node<T> // 선택 노드
+    
+}
