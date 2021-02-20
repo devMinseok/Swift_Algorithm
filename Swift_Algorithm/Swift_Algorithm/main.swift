242aa56210d89085284b9a4ad06ce279cab37d04
KangMinseok <qbq5000@gmail.com>
2021-02-20 23:58:07 +0900

    ✏️ 백준 알고리즘 공부 2일차

--- Swift_Algorithm/Swift_Algorithm/main.swift
+++ Swift_Algorithm/Swift_Algorithm/main.swift
@@ -30,3 +30,70 @@ func BEP() {
 }
 
 //BEP()
+
+// MARK: - 벌집
+func honeycomb() {
+    var num = Int(readLine()!)!
+    var cnt = 1
+    
+    while num > 1 {
+        num -= (6 * cnt)
+        cnt += 1
+    }
+    print(cnt)
+}
+
+//honeycomb()
+
+// MARK: - 분수찾기
+
+/*
+x/y 라고 할때
+
+if y == 1 {
+    if (x + y) == 짝수 {
+        오른쪽 위로
+    } else {
+        밑으로
+    }
+} else {
+    if (x + y) == 짝수 {
+        오른쪽으로
+    } else {
+        왼쪽 아래로
+    }
+}
+ */
+
+func findFraction() {
+    let num = Int(readLine()!)!
+    var x = 2
+    var y = 1
+  
+    if num <= 2 {
+        print("1/\(num)")
+        return
+    }
+    
+    for _ in 3..<num {
+        if y == 1 {
+            if (x + y) % 2 == 0 { //오른쪽 위로
+                x -= 1
+                y += 1
+            } else { // 밑으로
+                x += 1
+            }
+        } else {
+            if (x + y) % 2 == 0 { // 오른쪽으로
+                y += 1
+            } else { // 왼쪽 아래로
+                x += 1
+                y -= 1
+            }
+        }
+    }
+    
+    print("\(x)/\(y)")
+}
+
+findFraction()
