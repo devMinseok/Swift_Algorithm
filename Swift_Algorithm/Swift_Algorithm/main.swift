eb86834bb5628e7469611aa71546292f3a1dac52
KangMinseok <qbq5000@gmail.com>
2021-02-26 18:56:08 +0900

    ✏️ 백준 알고리즘 공부 6일차

--- Swift_Algorithm/Swift_Algorithm/main.swift
+++ Swift_Algorithm/Swift_Algorithm/main.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+// MARK: - 블랙잭
+func blackjack() {
+    
+    let nums = readLine()!.split(separator: " ").map { Int($0)! }
+    let N = nums[0]
+    let M = nums[1]
+    
+    let arr = readLine()!.split(separator: " ").map { Int($0)! }
+    var sum = 0
+    var max = 0
+    
+    for i in 0..<(N - 2) {
+        for j in (i + 1)..<(N - 1) {
+            for k in (j + 1)..<N {
+                sum = arr[i] + arr[j] + arr[k]
+                if sum == M {
+                    print(M)
+                    return
+                } else if sum < M {
+                    if sum > max {
+                        max = sum
+                    }
+                }
+            }
+        }
+    }
+    
+    print(max)
+}
+
+//blackjack()
