bbd742783c5c7a7664be7c2b348398ddcdd09cea
KangMinseok <qbq5000@gmail.com>
2021-03-10 23:37:17 +0900

    ✏️ 프로그래머스 알고리즘 공부 2일차

--- Swift_Algorithm/Swift_Algorithm/main.swift
+++ Swift_Algorithm/Swift_Algorithm/main.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+// MARK: - 스택 / 큐
+
+// MARK: - 다리를 지나는 트럭
+func solution(_ bridge_length: Int, _ weight: Int, _ truck_weights: [Int]) -> Int {
+    
+    
+    var bridge = Array(repeating: 0, count: bridge_length)
+    
+    
+    // truck_weight 배열의 각 요소를 bridge 배열에 넣어야 하는데, 각 트럭의 무게와 다리의 길이를 잘 고려하여 넣어야한다.
+    
+    
+    
+    
+    return 0
+}
+
+print(solution(2, 10, [7, 4, 5, 6]))
